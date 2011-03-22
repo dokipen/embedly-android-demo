3e8fac77f8ddc8bb0c01165ccda63f056934dee9
Bob Corsaro <bob@embed.ly>
2011-03-22 15:17:20 -0400

    Fixes formatting and cleans imports

--- src/com/embedly/android/demo/Demo.java
+++ src/com/embedly/android/demo/Demo.java
@@ -1,7 +1,6 @@
 package com.embedly.android.demo;
 
 import java.util.HashMap;
-import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -9,10 +8,8 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
 import android.text.ClipboardManager;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
@@ -34,65 +31,62 @@ public class Demo extends Activity {
 	private Button embedButton;
 	private EditText embedUrl;
 	private WebView embedWebView;
-	
-	private final static String LOADING = "<table width=100%25 height=100%25>"+
-	        "<tr><td valign=middle align=center><img "+
-	        "src=\"http://static.embed.ly/images/android-loader.gif\"/>" +
-	        "</td></tr></table>";
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
 
-    	embedResult = (TextView) findViewById(R.id.embedResult);
-    	embedButton = (Button) findViewById(R.id.embedButton);
-    	embedUrl = (EditText) findViewById(R.id.queryUrl);
-    	embedWebView = (WebView) findViewById(R.id.embedWebView);
-    	embedWebView.getSettings().setJavaScriptEnabled(true);
-    	embedWebView.getSettings().setPluginsEnabled(true);
-    	
-        embedButton.setOnClickListener(new OnClickListener() {
+	private final static String LOADING = "<table width=100%25 height=100%25>"
+			+ "<tr><td valign=middle align=center><img "
+			+ "src=\"http://static.embed.ly/images/android-loader.gif\"/>"
+			+ "</td></tr></table>";
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		embedResult = (TextView) findViewById(R.id.embedResult);
+		embedButton = (Button) findViewById(R.id.embedButton);
+		embedUrl = (EditText) findViewById(R.id.queryUrl);
+		embedWebView = (WebView) findViewById(R.id.embedWebView);
+		embedWebView.getSettings().setJavaScriptEnabled(true);
+		embedWebView.getSettings().setPluginsEnabled(true);
+
+		embedButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				fetchEmbed();
 			}
 		});
-        
-        registerForContextMenu(embedResult);
-		ClipboardManager clipboard = 
-			(ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+
+		registerForContextMenu(embedResult);
+		ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
 		clipboard.setText("http://www.flickr.com/photos/silent928/5550274021/");
-    }
-    
-    
-    
-    @Override
+	}
+
+	@Override
 	protected void onPause() {
 		super.onPause();
 		try {
-			Class.forName("android.webkit.WebView").
-			  getMethod("onPause", (Class[]) null).
-			  invoke(embedWebView, (Object[]) null);
+			Class.forName("android.webkit.WebView")
+					.getMethod("onPause", (Class[]) null)
+					.invoke(embedWebView, (Object[]) null);
 			embedWebView.resumeTimers();
 		} catch (Exception e) {
 			throw new RuntimeException("Unknown issue when killing flash", e);
 		}
 	}
 
-    @Override
+	@Override
 	protected void onResume() {
 		super.onResume();
 		try {
-			Class.forName("android.webkit.WebView").
-			  getMethod("onResume", (Class[]) null).
-			  invoke(embedWebView, (Object[]) null);
+			Class.forName("android.webkit.WebView")
+					.getMethod("onResume", (Class[]) null)
+					.invoke(embedWebView, (Object[]) null);
 			embedWebView.resumeTimers();
 		} catch (Exception e) {
 			throw new RuntimeException("Unknown issue when resuming flash", e);
 		}
 	}
-    
+
 	@Override
 	public void onCreateContextMenu(ContextMenu menu, View v,
 			ContextMenuInfo menuInfo) {
@@ -103,11 +97,10 @@ public class Demo extends Activity {
 
 	@Override
 	public boolean onContextItemSelected(MenuItem item) {
-		
+
 		switch (item.getItemId()) {
 		case R.id.copy_result:
-			ClipboardManager clipboard = 
-				(ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+			ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
 			clipboard.setText(embedResult.getText());
 			return true;
 		}
@@ -115,72 +108,71 @@ public class Demo extends Activity {
 	}
 
 	public void onEmbedResponse(JSONArray response) {
-    	try {
-            JSONObject obj = response.getJSONObject(0);
-            String type = obj.getString("type");
-    		StringBuffer embedBuf = new StringBuffer();
-    		
-            if ("photo".equals(type)) {
-            	if (!obj.isNull("url")) {
-            		embedBuf.append("<div>");
-            		embedBuf.append("<center><img width=\"100%25\" src=\"");
-            		embedBuf.append(obj.getString("url"));
-            		embedBuf.append("\"/></center></div>");
-            	}
-            } else if ("video".equals(type)) {
-	            if (!obj.isNull("html")) {
-	            	embedBuf.append(obj.getString("html"));
-	            }
-            } else if ("rich".equals(type)) {
-	            if (!obj.isNull("html")) {
-	            	embedBuf.append(obj.getString("html"));
-	            }
-            } else if ("link".equals(type)) {
-            	embedBuf.append("<a href=\"");
-            	embedBuf.append(obj.getString("url"));
-            	embedBuf.append("\">");
-	            if (!obj.isNull("title")) {
-	            	embedBuf.append(obj.getString("title"));
-	            } else {
-	            	embedBuf.append(obj.getString("url"));
-	            }
-	            embedBuf.append("</a>");
-            } else if ("error".equals(type)) {
-            	embedBuf.append("<p>");
-            	embedBuf.append(obj.getString("error_message"));
-            	embedBuf.append("</p>");
-            }
-    		
-            embedWebView.loadData(embedBuf.toString(), "text/html", 
-				"utf-8");
-            embedResult.setText(embedBuf.toString());
-        } catch(JSONException e) {
-        	throw new RuntimeException("Couldn't parse response", e);
-        }
-    }
-    
+		try {
+			JSONObject obj = response.getJSONObject(0);
+			String type = obj.getString("type");
+			StringBuffer embedBuf = new StringBuffer();
+
+			if ("photo".equals(type)) {
+				if (!obj.isNull("url")) {
+					embedBuf.append("<div>");
+					embedBuf.append("<center><img width=\"100%25\" src=\"");
+					embedBuf.append(obj.getString("url"));
+					embedBuf.append("\"/></center></div>");
+				}
+			} else if ("video".equals(type)) {
+				if (!obj.isNull("html")) {
+					embedBuf.append(obj.getString("html"));
+				}
+			} else if ("rich".equals(type)) {
+				if (!obj.isNull("html")) {
+					embedBuf.append(obj.getString("html"));
+				}
+			} else if ("link".equals(type)) {
+				embedBuf.append("<a href=\"");
+				embedBuf.append(obj.getString("url"));
+				embedBuf.append("\">");
+				if (!obj.isNull("title")) {
+					embedBuf.append(obj.getString("title"));
+				} else {
+					embedBuf.append(obj.getString("url"));
+				}
+				embedBuf.append("</a>");
+			} else if ("error".equals(type)) {
+				embedBuf.append("<p>");
+				embedBuf.append(obj.getString("error_message"));
+				embedBuf.append("</p>");
+			}
+
+			embedWebView.loadData(embedBuf.toString(), "text/html", "utf-8");
+			embedResult.setText(embedBuf.toString());
+		} catch (JSONException e) {
+			throw new RuntimeException("Couldn't parse response", e);
+		}
+	}
+
 	public void fetchEmbed() {
 
 		final Handler handler = new Handler();
-    	final Demo self = this;
-    	
+		final Demo self = this;
+
 		handler.post(new Runnable() {
-			
+
 			public void run() {
-		    	embedWebView.loadData(LOADING, "text/html", "utf-8");
-		    	embedResult.setText("");
-				
-		    	InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-		    	mgr.hideSoftInputFromWindow(embedUrl.getWindowToken(), 0);
+				embedWebView.loadData(LOADING, "text/html", "utf-8");
+				embedResult.setText("");
+
+				InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+				mgr.hideSoftInputFromWindow(embedUrl.getWindowToken(), 0);
 			}
 		});
 
-    	String url = embedUrl.getText().toString();
-        final HashMap<String, Object> params = new HashMap<String, Object>();
-        params.put("url", url);
-        params.put("maxwidth", "300");
-		
-    	new Thread() {
+		String url = embedUrl.getText().toString();
+		final HashMap<String, Object> params = new HashMap<String, Object>();
+		params.put("url", url);
+		params.put("maxwidth", "300");
+
+		new Thread() {
 			@Override
 			public void run() {
 				final JSONArray response = api.oembed(params);
@@ -190,7 +182,7 @@ public class Demo extends Activity {
 					}
 				});
 			}
-    	}.start();
-    	
-    }
+		}.start();
+
+	}
 }
